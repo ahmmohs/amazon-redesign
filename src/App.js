71767451b915d2462e59b5f0c2ba5e17bf59979a
awsaf <awsafsikdar@gmail.com>
2020-09-11 18:14:51 -0400

    header

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles/styles.css'
+
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      {/*Landing product display*/}
+      {/*Categories/Products Display*/}
+    </div>
+  );
+}
+
+export default App;
